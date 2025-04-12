06c7c7325d96182c40a58d0b79a20220d57dbdde
Harryson <168111404+Harryson888@users.noreply.github.com>
2025-04-11 19:56:06 -0500

    Delete KoalGroup-Harry directory

--- KoalGroup-Harry/produccion_inventario/apps.py
+++
@@ -1,9 +0,0 @@
-# produccion_inventario/apps.py
-from django.apps import AppConfig
-
-class ProduccionInventarioConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'produccion_inventario'
-
-    def ready(self):
-        import produccion_inventario.signals  # Importa las señales
